b0c037bf54e8d038608f9436eea0055913e131cd
La Luz <127034113+dinhcodes@users.noreply.github.com>
2026-03-25 11:09:40 +0800

    Ui: Update FilterPanelField
    Currently, FilterPanelField accepts a Consumer<Set<String>> in order
    to get its tags updated from higher-level components.
    Let's:
    * Define a KeywordSubmittedHandler functional interface to replace
    the Consumer<Set<String>>.
    * KeywordSubmittedHandlers will take in a String keyword and return
    a List<String> of tags
    
    List<String> is now used in lieu of Set<String> to preserve the order
    of tags. This is because the order of tags in the FilterPanel is
    important for the user experience.

--- src/main/java/seedu/address/ui/filter/FilterPanelField.java
+++ src/main/java/seedu/address/ui/filter/FilterPanelField.java
@@ -1,4 +1,74 @@
 package seedu.address.ui.filter;
 
-public class FilterPanelField {
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Region;
+import seedu.address.ui.UiPart;
+
+/**
+ * Reusable filter field in the {@code FilterPanel}.
+ */
+public class FilterPanelField extends UiPart<Region> {
+	private static final String FXML = "FilterPanelField.fxml";
+
+	private final KeywordSubmittedHandler onKeywordSubmitted;
+
+	@FXML
+	private Label titleLabel;
+	@FXML
+	private TextField keywordInputField;
+	@FXML
+	private Label keywordsLabel;
+	@FXML
+	private FlowPane keywordsFlowPane;
+
+	/**
+	 * Creates a reusable filter field section.
+	 */
+	public FilterPanelField(String title, String promptText, KeywordSubmittedHandler onKeywordSubmitted) {
+		super(FXML);
+		requireNonNull(title);
+		requireNonNull(promptText);
+		this.onKeywordSubmitted = requireNonNull(onKeywordSubmitted);
+		titleLabel.setText(title);
+		keywordInputField.setPromptText(promptText);
+	}
+
+	/**
+	 * Replaces currently displayed tags with the provided keywords.
+	 */
+	public void setKeywords(List<String> keywords) {
+		requireNonNull(keywords);
+		renderKeywords(keywords);
+	}
+
+	@FXML
+	private void handleFieldEntered() {
+		String keyword = keywordInputField.getText();
+        String trimmedKeyword = keyword.trim();
+
+        if (trimmedKeyword == null || trimmedKeyword.isEmpty()) {
+			return;
+		}
+
+        List<String> listOfKeywords = onKeywordSubmitted.handle(trimmedKeyword);
+        renderKeywords(listOfKeywords);
+		keywordInputField.clear();
+	}
+
+	private void renderKeywords(List<String> keywords) {
+		keywordsFlowPane.getChildren().clear();
+		keywords.forEach(keyword -> keywordsFlowPane.getChildren().add(new FilterPanelTag(keyword).getRoot()));
+	}
+
+    @FunctionalInterface
+    public interface KeywordSubmittedHandler {
+        List<String> handle(String keyword);
+    }
 }
